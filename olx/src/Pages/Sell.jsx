131c43d116ac906db082b904043835e900066855
anshg03 <99557054+anshg03@users.noreply.github.com>
2023-07-03 00:06:15 +0530

    Update Sell.jsx

--- olx/src/Pages/Sell.jsx
+++ olx/src/Pages/Sell.jsx
@@ -1,136 +1,157 @@
-import {
-  Box,
-  Button,
-  Checkbox,
-  FormControl,
-  FormLabel,
-  GridItem,
-  Heading,
-  Input,
-  Select,
-  SimpleGrid,
-  Text,
-  Textarea,
-  useColorModeValue,
-  VStack,
+import{Box,
+Button,
+Checkbox,
+FormControl,
+FormLabel,
+GridItem,
+Heading,
+Input,
+Select,
+SimpleGrid,
+Text,
+Textarea,
+useColorModeValue,
+VStack,
+InputGroup,
+InputLeftAddon,
+InputRightAddon,
+Center
 } from "@chakra-ui/react";
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
+
 const Sell = () => {
-  const [name, setname] = useState("");
+const [details, setDetails] = useState({
+  name: "",
+  email: "",
+  description: "",
+  amount: 0,
+  proImage: [],
+  phoneNo: ""
+});
+const handleFileUpload = (event) => {
+  const files = event.target.files;
+  if (files.length > 5) {
+    console.log("Maximum 5 files can be uploaded.");
+    return;
+  }
+  setDetails({
+    ...details,
+    proImage: files
+  });
+};
+const handleDetailChange = (e) => {
+  const { name, value } = e.target;
+  setDetails({
+    ...details,
+    [name]: value
+  })
+  console.log(details);
+}
 
-  const handleNameChange = (event) => {
-    const inputName = event.target.value;
-    if (inputName.length <= 30) {
-      setname(inputName);
-    }
-  };
-
-  const handleFileUpload = (event) => {
-    const files = event.target.files;
-    if (files.length > 5) {
-      console.log("Maximum 5 files can be uploaded.");
-      return;
-    }
-  };
-
-  return (
-    <VStack w="100%" p={100} spacing={10}>
-      <Heading
-        textAlign="center"
-        fontSize="4xl"
-        bg={useColorModeValue("gray.100", "gray.900")}
+return (
+  <VStack w="100%" p={50} spacing={10}>
+    <Heading
+      textAlign="center"
+      fontSize="4xl"
+      bg={useColorModeValue("gray.50", "gray.900")}
+    >
+      POST YOUR AD
+    </Heading>
+    <Box
+      bg={useColorModeValue("gray.100", "gray.800")}
+      p={8}
+      borderRadius="lg"
+      boxShadow={"xl"}
+      rounded={"lg"}
+      w="full"
+    >
+      <SimpleGrid
+        columns={2}
+        columnGap={10}
+        rowGap={10}
+        alignItems="flex-start"
       >
-        POST YOUR AD
-      </Heading>
-      <Box
-        bg={useColorModeValue("white", "gray.800")}
-        p={8}
-        borderRadius="lg"
-        boxShadow={"lg"}
-        rounded={"lg"}
-        w="full"
-      >
-        <SimpleGrid
-          columns={2}
-          columnGap={2}
-          rowGap={4}
-          alignItems="flex-start"
-        >
-          <GridItem colSpan={2}>
-            <FormControl>
-              <FormLabel fontSize="3xl">Field *</FormLabel>
-              <Textarea placeholder="Enter the field..." fontSize="xl" />
-              <Text fontSize="xl">
-                Mention the field of the item (e.g., cycle, etc)
-              </Text>
-            </FormControl>
-          </GridItem>
-          <GridItem colSpan={2}>
-            <FormControl>
-              <FormLabel fontSize="3xl">Description *</FormLabel>
-              <Textarea placeholder="Enter description..." fontSize="xl" />
-              <Text fontSize="xl">
-                Includes the condition and features of your item
-              </Text>
-            </FormControl>
-          </GridItem>
-          <GridItem colSpan={2}>
-            <FormControl>
-              <FormLabel fontSize="3xl">Price *</FormLabel>
-              <Input
-                type="text"
-                placeholder="Enter price in rupees..."
-                fontSize="xl"
-              />
-            </FormControl>
-          </GridItem>
-          <GridItem colSpan={2}>
-            <FormControl>
-              <FormLabel fontSize="3xl">Upload Images *</FormLabel>
-              <Input
-                type="file"
-                onChange={handleFileUpload}
-                multiple
-                accept="image/*"
-                max="5"
-              />
-              <Text fontSize="xl">Select up to 5 images</Text>
-            </FormControl>
-          </GridItem>
-        </SimpleGrid>
-      </Box>
-      <Heading
-        textAlign="center"
-        fontSize="4xl"
-        bg={useColorModeValue("gray.100", "gray.900")}
-      >
-        REVIEW YOUR DETAILS
-      </Heading>
-      <Box
-        bg={useColorModeValue("white", "gray.800")}
-        p={8}
-        borderRadius="lg"
-        boxShadow={"lg"}
-        rounded={"lg"}
-        w="full"
-      >
-        <FormControl>
-          <FormLabel fontSize="3xl">Name</FormLabel>
-          <Input
-            type="text"
-            placeholder="Your Name"
-            fontSize="xl"
-            value={name}
-            onChange={handleNameChange}
-          />
-          <Text fontSize="xl" textAlign="right">
-            {`${name.length}/30`}
-          </Text>
-        </FormControl>
-      </Box>
-    </VStack>
-  );
+        <GridItem colSpan={[2, 2, 1]}>
+          <FormControl>
+            <FormLabel fontSize="3xl">Name of the product *</FormLabel>
+            <Input
+              type="text"
+              placeholder="Enter price in rupees..."
+              fontSize="xl"
+              name="name"
+              value={details.name}
+              onChange={handleDetailChange}
+            />
+            <Text fontSize="xl">
+              Mention the name of the item (e.g., cycle, etc)
+            </Text>
+          </FormControl>
+        </GridItem>
+        <GridItem colSpan={[2, 2, 1]}>
+          <FormControl>
+            <FormLabel fontSize="3xl">Description *</FormLabel>
+            <Textarea placeholder="Enter description..." fontSize="xl" rows={1} name="description" value={details.description} onChange={handleDetailChange} />
+            <Text fontSize="xl">
+              Includes the condition and features of your item
+            </Text>
+          </FormControl>
+        </GridItem>
+        <GridItem colSpan={[2, 2, 1]}>
+          <FormControl>
+            <FormLabel fontSize="3xl">Price *</FormLabel>
+            <Input
+              type="text"
+              placeholder="Enter price in rupees..."
+              fontSize="xl"
+              name="amount"
+              value={details.amount}
+              onChange={handleDetailChange}
+            />
+          </FormControl>
+        </GridItem>
+        <GridItem colSpan={[2, 2, 1]}>
+          <FormControl>
+            <FormLabel fontSize="3xl">Upload Images *</FormLabel>
+            <Input
+              type="file"
+              onChange={handleFileUpload}
+              multiple
+              accept="image/*"
+              max="5"
+              name="proImage"
+            />
+            <Text fontSize="xl">
+              Select up to 5 images
+            </Text>
+          </FormControl>
+        </GridItem>
+        <GridItem colSpan={[2, 2, 1]}>
+          <FormControl>
+            <FormLabel fontSize="3xl">Email *</FormLabel>
+            <InputGroup>
+              <Input type="email" placeholder='Email' fontSize="xl" name="email" value={details.email} onChange={handleDetailChange} />
+              <InputRightAddon children='.com' fontSize="xl" />
+            </InputGroup>
+          </FormControl>
+        </GridItem>
+        <GridItem colSpan={[2, 2, 1]}>
+          <FormControl>
+            <FormLabel fontSize="3xl">Phone Number *</FormLabel>
+            <InputGroup fontSize="3xl">
+              <InputLeftAddon children='+91' fontSize="xl" />
+              <Input type='tel' placeholder='Phone number' fontSize="xl" name="phoneNo" value={details.phoneNo} onChange={handleDetailChange} />
+            </InputGroup>
+          </FormControl>
+        </GridItem>
+        <GridItem colSpan={2}>
+          <Center>
+            <Button size="lg">Submit</Button>
+          </Center>
+        </GridItem>
+      </SimpleGrid>
+    </Box>
+  </VStack>
+);
 };
 
 export default Sell;
